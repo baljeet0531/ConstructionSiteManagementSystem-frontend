0485aaa8e347dc90f75a2c0bbf6c1b2b28753ead
lhleung <grandq33769@gmail.com>
2023-01-12 18:19:46 +0800

    feat(menu): organize and add feature template

---
+++ src/Components/MachineryEstablishment/MachineryEstablishment.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { IsPermit } from '../../Mockdata/Mockdata';
+
+export default function MachineryEstablishment() {
+    if (!IsPermit('machinery_establishment')) return <Navigate to="/" replace={true} />;
+
+    return <p>機具檢點建置</p>;
+}
