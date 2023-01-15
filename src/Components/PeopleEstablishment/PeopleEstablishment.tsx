bafd90b559b39945277f58604ebc76d979d6edf8
baljeet0531 <barryfang0531@gmail.com>
2023-01-15 22:08:58 +0800

    feat(people-establishment): change HumanResource to PeopleEstablishment and add success toast

--- src/Components/PeopleEstablishment/PeopleEstablishment.tsx
+++ src/Components/PeopleEstablishment/PeopleEstablishment.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { IsPermit } from '../../Mockdata/Mockdata';
+import BuildFormik from './BuildFormik';
 
 export default function PeopleEstablishment() {
-    if (!IsPermit('people_establishment')) return <Navigate to="/" replace={true} />;
+    if (!IsPermit('people_establishment'))
+        return <Navigate to="/" replace={true} />;
 
-    return <p>人員資料建置</p>;
+    return <BuildFormik></BuildFormik>;
 }
