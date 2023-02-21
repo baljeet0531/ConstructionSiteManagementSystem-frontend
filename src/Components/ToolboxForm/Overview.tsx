05da48dba9340b38dbd75bc5c9d826eb218467ff
baljeet0531 <barryfang0531@gmail.com>
2023-02-21 14:42:00 +0800

    feat(toolbox-overview): add toolbox query, define toolbox columnmap and sizes, and move common elements to react-window-table

--- src/Components/ToolboxForm/Overview.tsx
+++ src/Components/ToolboxForm/Overview.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { IsPermit } from '../../Mockdata/Mockdata';
 import {
     Flex,
     Text,
@@ -11,10 +13,82 @@ import {
     Grid,
 } from '@chakra-ui/react';
 import { DateRangePicker } from 'rsuite';
-import { Navigate } from 'react-router-dom';
-import { IsPermit } from '../../Mockdata/Mockdata';
-import { ArrowDropDownIcon, LaunchIcon } from '../../Icons/Icons';
 import { DateRange } from 'rsuite/esm/DateRangePicker/types';
+import { ArrowDropDownIcon, LaunchIcon } from '../../Icons/Icons';
+import PageLoading from '../Shared/PageLoading';
+import { gql, useQuery } from '@apollo/client';
+import { SIGNATURE_FIELDS } from '../../Utils/GQLFragments';
+import { IGQLSignature } from '../../Interface/Signature';
+import ReactWindowTable, {
+    IColumnMap,
+    ISizes,
+    getElementProps,
+    defaultElement,
+    CheckboxElement,
+    SignatureStatusElement,
+} from '../Shared/ReactWindowTable';
+
+const QUERY_TOOLBOX = gql`
+    ${SIGNATURE_FIELDS}
+    query ToolboxMeeting(
+        $siteId: String!
+        $number: String
+        $startDate: DateTime
+        $endDate: DateTime
+    ) {
+        toolboxMeeting(
+            siteId: $siteId
+            number: $number
+            startDate: $startDate
+            endDate: $endDate
+        ) {
+            number
+            system
+            systemBranch
+            project
+            area
+            laborAmount
+            meetingDatetime
+            contractingCorpStaffSignatureFirst {
+                ...gqlSignatureFields
+            }
+            contractingCorpStaffSignatureSecond {
+                ...gqlSignatureFields
+            }
+            contractingCorpStaffSignatureThird {
+                ...gqlSignatureFields
+            }
+            systemEngineerSignature {
+                ...gqlSignatureFields
+            }
+        }
+    }
+`;
+
+interface IToolboxOverview {
+    number: string;
+    system: string;
+    systemBranch: string;
+    project: string;
+    area: string;
+    laborAmount: number;
+    meetingDatetime: string;
+    contractingCorpStaffSignatureFirst: IGQLSignature;
+    contractingCorpStaffSignatureSecond: IGQLSignature;
+    contractingCorpStaffSignatureThird: IGQLSignature;
+    systemEngineerSignature: IGQLSignature;
+}
+
+interface IToolboxOverviewChecked extends IToolboxOverview {
+    index: number;
+    isChecked: boolean;
+}
+
+const sizes: ISizes = {
+    tableFigmaWidth: 877,
+    headerHeight: 44,
+    cellHeight: 44,
+};
 
 export default function ToolboxFormOverview(props: {
     siteId: string;
@@ -22,9 +96,135 @@ export default function ToolboxFormOverview(props: {
 }) {
     if (!IsPermit('eng_toolbox_form'))
         return <Navigate to="/" replace={true} />;
-    const { siteName } = props;
+    const columnMap: IColumnMap[] = [
+        {
+            title: '日期',
+            width: 99,
+            variable: 'meetingDatetime',
+            getElement: defaultElement,
+        },
+        {
+            title: '單號',
+            width: 83,
+            variable: 'number',
+            getElement: defaultElement,
+        },
+        {
+            title: '系統',
+            width: 100,
+            variable: 'system',
+            getElement: defaultElement,
+        },
+        {
+            title: '系統分類',
+            width: 100,
+            variable: 'systemBranch',
+            getElement: defaultElement,
+        },
+        {
+            title: '施工項目',
+            width: 100,
+            variable: 'project',
+            getElement: defaultElement,
+        },
+        {
+            title: '施工廠區',
+            width: 95,
+            variable: 'area',
+            getElement: defaultElement,
+        },
+        {
+            title: '簽核狀態',
+            width: 180,
+            variable: 'signatureStatus',
+            getElement: (props: getElementProps) => {
+                const {
+                    contractingCorpStaffSignatureFirst,
+                    contractingCorpStaffSignatureSecond,
+                    contractingCorpStaffSignatureThird,
+                    systemEngineerSignature,
+                } = props.info as IToolboxOverviewChecked;
+                const signatureFieldList = [
+                    {
+                        signature: contractingCorpStaffSignatureFirst,
+                        fieldLabel: '承商人員',
+                    },
+                    {
+                        signature: contractingCorpStaffSignatureSecond,
+                        fieldLabel: '承商人員',
+                    },
+                    {
+                        signature: contractingCorpStaffSignatureThird,
+                        fieldLabel: '承商人員',
+                    },
+                    {
+                        signature: systemEngineerSignature,
+                        fieldLabel: '系統工程師',
+                    },
+                ];
 
+                return (
+                    <SignatureStatusElement
+                        getElementProps={props}
+                        signatureFieldList={signatureFieldList}
+                    ></SignatureStatusElement>
+                );
+            },
+        },
+        {
+            title: '出工人數',
+            width: 70,
+            variable: 'laborAmount',
+            getElement: defaultElement,
+        },
+        {
+            title: '全選',
+            width: 50,
+            variable: 'isChecked',
+            getElement: (props: getElementProps) => (
+                <CheckboxElement
+                    getElementProps={props}
+                    setTableData={setTableData}
+                    primaryKey={'number'}
+                ></CheckboxElement>
+            ),
+        },
+    ];
+
+    const { siteId, siteName } = props;
     const [dateRange, setDateRange] = React.useState<DateRange | null>(null);
+
+    const [tableData, setTableData] = React.useState<{
+        [number: string]: IToolboxOverviewChecked;
+    }>({});
+
+    console.log(tableData);
+
+    const { loading } = useQuery(QUERY_TOOLBOX, {
+        variables: {
+            siteId: siteId,
+        },
+        onCompleted: ({
+            toolboxMeeting,
+        }: {
+            toolboxMeeting: IToolboxOverview[];
+        }) => {
+            const data = toolboxMeeting.map((info, index) => ({
+                [info.number]: {
+                    ...info,
+                    meetingDatetime: info.meetingDatetime.split('T')[0],
+                    index: index + 1,
+                    isChecked: false,
+                },
+            }));
+            const dataObject = Object.assign({}, ...data);
+            setTableData(dataObject);
+        },
+        onError: (err) => {
+            console.log(err);
+        },
+        fetchPolicy: 'network-only',
+    });
 
     return (
         <Flex
@@ -141,19 +341,12 @@ export default function ToolboxFormOverview(props: {
                     輸出
                 </Button>
             </Flex>
-            {/* {(loading || searchLoading || exportLoading) && (
-                <Center
-                    position={'absolute'}
-                    top={0}
-                    left={'20vw'}
-                    w={'80vw'}
-                    h={'100vh'}
-                    bg={'#D9D9D980'}
-                    zIndex={2}
-                >
-                    <Spinner size={'xl'} />
-                </Center>
-            )} */}
+            <ReactWindowTable
+                tableData={tableData}
+                columnMap={columnMap}
+                sizes={sizes}
+            />
+            {loading && <PageLoading />}
         </Flex>
     );
 }
