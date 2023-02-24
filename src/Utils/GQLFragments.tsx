b121f545977870c09b48ae56c5ebaaf256a0af28
lhleung <grandq33769@gmail.com>
2023-02-24 13:25:42 +0800

    fix(work-permit): use gqlFragment and use new signature structure

--- src/Utils/GQLFragments.tsx
+++ src/Utils/GQLFragments.tsx
@@ -1,17 +1,22 @@
 import { gql } from '@apollo/client';
 
+// "__typename" must be added in the last line of fragment
 export const SIGNATURE_FIELDS = gql`
     fragment gqlSignatureFields on gqlSignature {
+        no
         path
         time
         owner
+        __typename
     }
-`
+`;
 
 export const APPEARANCE_SIGN_FIELD = gql`
-fragment gqlAppearFields on gqlAppearanceSignature {
-    path
-    time
-    owner
-}
+    fragment appearFields on gqlAppearanceSignature {
+        no
+        path
+        time
+        owner
+        __typename
+    }
 `;
