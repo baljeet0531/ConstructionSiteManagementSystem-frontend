479467e6c32474ee690ed1b0ffef591cf5341780
lhleung <grandq33769@gmail.com>
2023-03-04 01:27:42 +0800

    feat(opCheck): complete the header of the form

--- src/Components/OpCheckForm/Factory.tsx
+++ src/Components/OpCheckForm/Factory.tsx
@@ -1,15 +1,22 @@
-import { Input } from '@chakra-ui/react';
+import { Checkbox, GridItem, Input } from '@chakra-ui/react';
 import { FormikProps } from 'formik';
-import { IOpCheck } from '../../Interface/OpCheck/Common';
+import { IOpCheck, OpCheckName } from '../../Interface/OpCheck/Common';
 import { OpCheckHandler } from '../../Utils/OpCheck/Handler';
+import { opCheckMap } from '../../Utils/OpCheck/Mapper';
 import { placeholderStyle } from './Style';
 
 export default class FormFactory {
     formProps: FormikProps<IOpCheck>;
+    type: OpCheckName;
     handler: OpCheckHandler;
 
-    constructor(formProps: FormikProps<IOpCheck>, handler: OpCheckHandler) {
+    constructor(
+        formProps: FormikProps<IOpCheck>,
+        type: OpCheckName,
+        handler: OpCheckHandler
+    ) {
         this.formProps = formProps;
+        this.type = type;
         this.handler = handler;
     }
 
@@ -17,10 +24,31 @@ export default class FormFactory {
         return (
             <Input
                 type="text"
-                border="0px"
+                size="sm"
                 placeholder="填寫"
                 _placeholder={placeholderStyle}
             />
+        );
+    }
+    typeCheckBox(type: OpCheckName, label: string) {
+        return (
+            <GridItem>
+                <Checkbox
+                    w="100%"
+                    h="100%"
+                    spacing="0.5rem"
+                    size="sm"
+                    isChecked={type === this.type}
+                    disabled
+                >
+                    {label}
+                </Checkbox>
+            </GridItem>
+        );
+    }
+    allTypeCheckBoxes() {
+        return Object.entries(opCheckMap).map(([key, item]) =>
+            this.typeCheckBox(key as OpCheckName, item.name)
         );
     }
 }
--- src/Components/OpCheckForm/Form.tsx
+++ src/Components/OpCheckForm/Form.tsx
@@ -1,21 +1,34 @@
 import { useQuery } from '@apollo/client';
-import { Box, Button, VStack, Text } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    VStack,
+    Text,
+    Image,
+    Grid,
+    GridItem,
+    Input,
+} from '@chakra-ui/react';
 import { Form, FormikProps } from 'formik';
 import { EditIcon } from '../../Icons/Icons';
-import { IOpCheck } from '../../Interface/OpCheck/Common';
+import { IOpCheck, OpCheckName } from '../../Interface/OpCheck/Common';
 import { OpCheckHandler } from '../../Utils/OpCheck/Handler';
 import { FormLoading } from '../Shared/Loading';
 import FormFactory from './Factory';
+import GridInputItem from '../Shared/GridInputItem';
+import { unboxStyle } from './Style';
 
 export default function OpCheckForm({
     formProps,
+    type,
     handler,
 }: {
     formProps: FormikProps<IOpCheck>;
+    type: OpCheckName;
     handler: OpCheckHandler;
 }) {
     document.title = `特殊作業工安自主檢點表(${handler.number})`;
-    const f = new FormFactory(formProps, handler);
+    const f = new FormFactory(formProps, type, handler);
     const { loading } = useQuery(handler.query, {
         variables: {
             siteId: handler.siteId,
@@ -47,6 +60,7 @@ export default function OpCheckForm({
                 完成編輯
             </Button>
             <Box margin="10px 37px 0px 27px" color="#667080">
+                <Image w="150px" h="51px" src={'/mic-icon.png'} />
                 <VStack
                     mt="-30px"
                     spacing="-4px"
@@ -57,8 +71,54 @@ export default function OpCheckForm({
                     <Text>Marketech International Corp.</Text>
                     <Text>特殊作業工安自主檢點表</Text>
                 </VStack>
+                <Grid
+                    height="80px"
+                    w="90%"
+                    mt="30px"
+                    templateColumns="6em repeat(4, 1fr)"
+                    templateRows="repeat(3, 1fr)"
+                >
+                    <GridItem {...unboxStyle}>作業名稱：</GridItem>
+                    <GridItem rowStart={2} rowEnd={4} />
+                    {f.allTypeCheckBoxes()}
+                </Grid>
+                <Grid
+                    w="100%"
+                    mt="15px"
+                    templateColumns="repeat(2, 80px 2fr 50px 2fr) "
+                >
+                    <GridItem {...unboxStyle}>施工地點：</GridItem>
+                    <GridInputItem
+                        fieldName="area"
+                        inputComponent={f.textInput()}
+                        style={{ ...unboxStyle }}
+                    />
+                    <GridItem {...unboxStyle} justifyContent="flex-end">
+                        廠區：
+                    </GridItem>
+                    <GridInputItem
+                        fieldName="zone"
+                        inputComponent={f.textInput()}
+                        style={{ ...unboxStyle }}
+                    />
+                    <GridItem {...unboxStyle} justifyContent="flex-end">
+                        作業單位：
+                    </GridItem>
+                    <GridInputItem
+                        fieldName="department"
+                        inputComponent={f.textInput()}
+                        style={{ ...unboxStyle }}
+                    />
+                    <GridItem {...unboxStyle} justifyContent="flex-end">
+                        日期：
+                    </GridItem>
+                    <GridInputItem
+                        fieldName="day"
+                        inputComponent={<Input type="date" size="sm" />}
+                        style={{ ...unboxStyle }}
+                    />
+                </Grid>
             </Box>
-            {f.textInput()}
             {(loading || formProps.isSubmitting) && <FormLoading />}
         </Form>
     );
