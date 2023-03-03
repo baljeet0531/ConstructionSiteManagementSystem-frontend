09dde9a99d5988e1ce940958189caec4b1ef9b46
lhleung <grandq33769@gmail.com>
2023-03-04 01:27:42 +0800

    feat(opCheck): complete the header of the form

--- src/Components/OpCheckForm/Style.tsx
+++ src/Components/OpCheckForm/Style.tsx
@@ -1,3 +1,16 @@
+export const baseStyle = {
+    border: '1px',
+    borderColor: '#919AA9',
+    display: 'flex',
+    alignItems: 'center',
+};
+
+export const unboxStyle = {
+    ...baseStyle,
+    border: '0px',
+    letterSpacing: '0.05em',
+};
+
 export const placeholderStyle = {
     color: '#667080',
     opacity: 0.5,
