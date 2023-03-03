5a8ec6cc76b840294b1d3ec48459ee8b7b794fb6
lhleung <grandq33769@gmail.com>
2023-03-03 23:06:14 +0800

    feat(opCheck): initialize the structure of opCheckForm

---
+++ src/Components/OpCheckForm/Overview.tsx
@@ -0,0 +1,7 @@
+export default function OpCheckOverview() {
+    localStorage.setItem(
+        'singleOpCheckObject',
+        JSON.stringify({ number: '230206-24', type: 'fire' })
+    );
+    return <></>;
+}
