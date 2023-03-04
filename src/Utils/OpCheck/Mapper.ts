d7b79ca30bb6625d598fa1c2c4fa7bbf33825ddd
lhleung <grandq33769@gmail.com>
2023-03-04 16:33:58 +0800

    feat(opCheck): finish Electric opCheck

--- src/Utils/OpCheck/Mapper.ts
+++ src/Utils/OpCheck/Mapper.ts
@@ -5,6 +5,7 @@ import { OpCheckHandler } from './Handler';
 import { AssembleOpCheckHandler } from './Type/Assemble';
 import { CageOpCheckHandler } from './Type/Cage';
 import { ChemicalOpCheckHandler } from './Type/Chemical';
+import { ElectricOpCheckHandler } from './Type/Electric';
 import { FireOpCheckHandler } from './Type/Fire';
 
 type OpCheckConstructor = new (
@@ -23,7 +24,7 @@ export const opCheckMap: Record<OpCheckName, OpCheckMapItem> = {
     cage: { name: '吊籠作業', handler: CageOpCheckHandler },
     chemical: { name: '化學作業', handler: ChemicalOpCheckHandler },
     confineSpace: { name: '侷限空間作業', handler: FireOpCheckHandler },
-    electric: { name: '電力作業', handler: FireOpCheckHandler },
+    electric: { name: '電力作業', handler: ElectricOpCheckHandler },
     fire: { name: '動火作業', handler: FireOpCheckHandler },
     hole: { name: '開口作業', handler: FireOpCheckHandler },
     lift: { name: '起重吊掛作業', handler: FireOpCheckHandler },
