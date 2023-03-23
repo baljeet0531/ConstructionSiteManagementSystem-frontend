064c52abbd7206b40c70c90d31bb36f7c2a19d26
lhleung <grandq33769@gmail.com>
2023-03-23 16:31:15 +0800

    feat(dailyReport): finish basic information section

--- src/Components/Report/Daily/Factory.tsx
+++ src/Components/Report/Daily/Factory.tsx
@@ -1,10 +1,25 @@
 import { IDailyReport } from '../../../Interface/DailyReport';
 import { FormikProps } from 'formik';
+import { Input } from '@chakra-ui/react';
+import { placeholderStyle, disabledStyle } from './Styles';
 
 export default class FormFactory {
     formProps: FormikProps<IDailyReport>;
 
     constructor(formProps: FormikProps<IDailyReport>) {
         this.formProps = formProps;
+    }
+
+    input(type: string, disabled: boolean = false) {
+        return (
+            <Input
+                type={type}
+                size="sm"
+                placeholder="填寫"
+                _placeholder={placeholderStyle}
+                disabled={disabled}
+                _disabled={disabledStyle}
+            />
+        );
     }
 }
---
+++ src/Components/Report/Daily/Styles.tsx
@@ -0,0 +1,24 @@
+export const titleStyle = {
+    display: 'flex',
+    alignItems: 'flex-end',
+};
+
+export const subTitleStyle = {
+    fontSize: '24px',
+};
+
+export const placeholderStyle = {
+    color: '#667080',
+    opacity: 0.5,
+};
+
+export const disabledStyle = {
+    background: 'rgba(102, 112, 128, 0.1)',
+    color: '#667080',
+    border: '0px',
+};
+
+export const contentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+};
