a7b23ef1ce6535925a16ca866939be4299777de7
lhleung <grandq33769@gmail.com>
2023-03-24 15:53:13 +0800

    feat(dailyReport): finish workNumber table

--- src/Interface/DailyReport.tsx
+++ src/Interface/DailyReport.tsx
@@ -2,3 +2,25 @@ export interface IDailyReport {
     siteId: string;
     serialNumber: string;
 }
+export type TCategory =
+    | '帆宣'
+    | '空調'
+    | '消防'
+    | '給排水'
+    | '天然氣及柴油'
+    | '電力'
+    | '儀控'
+    | '弱電'
+    | '其他'
+    | '合計';
+
+export interface IWorkNumber {
+    supervisor?: IWorkNumberItem
+    worker?: IWorkNumberItem
+    overtime?: IWorkNumberItem
+    sum: IWorkNumberItem
+}
+
+export interface IWorkNumberItem {
+    fieldName: string;
+}
