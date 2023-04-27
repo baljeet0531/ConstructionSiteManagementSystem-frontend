addf696d67a22fcdbba075a70fe0c25dde74db92
baljeet0531 <barryfang0531@gmail.com>
2023-04-27 18:06:13 +0800

    feat(daily-report): complete "query data" and "export" feature, and link edit button to modal

--- src/Components/Report/Report.tsx
+++ src/Components/Report/Report.tsx
@@ -10,17 +10,67 @@ import {
     TabPanels,
     Tabs,
     Text,
+    useToast,
 } from '@chakra-ui/react';
 import { tableViewContainerStyle } from '../../Layouts/MainScreen/MainScreen';
 import { DateRangePicker } from 'rsuite';
 import { LaunchIcon } from '../../Icons/Icons';
-import DailyReport from './DailyReport';
+import DailyReport, { IDailyReportObject } from './Daily/Overview';
+import { gql, useMutation } from '@apollo/client';
+import { Cookies } from 'react-cookie';
+import { exportFile } from '../../Utils/Resources';
+import { defaultErrorToast } from '../../Utils/DefaultToast';
+import { PageLoading } from '../Shared/Loading';
+import { DateRange } from 'rsuite/esm/DateRangePicker';
+
+const EXPORT_DAILY_REPORT = gql`
+    mutation ExportDailyReport(
+        $dailyIds: [Int]!
+        $siteId: String!
+        $username: String!
+    ) {
+        exportDailyReport(
+            dailyIds: $dailyIds
+            siteId: $siteId
+            username: $username
+        ) {
+            ok
+            message
+            path
+        }
+    }
+`;
 
 export default function Report(props: { siteId: string; siteName: string }) {
     if (!IsPermit('project_report')) return <Navigate to="/" replace={true} />;
-    const { siteName } = props;
+    const { siteId, siteName } = props;
+    const username = new Cookies().get('username');
+    const toast = useToast();
 
     const [tabIndex, setTabIndex] = React.useState<number>(0);
+    const [fileLoading, setFileLoading] = React.useState<boolean>(false);
+    const [dailyData, setDailyData] = React.useState<IDailyReportObject>({});
+    const [dateRange, setDateRange] = React.useState<DateRange | null>(null);
+
+    const [exportDailyReport, { loading: exportDailyLoading }] = useMutation(
+        EXPORT_DAILY_REPORT,
+        {
+            onCompleted: ({ exportDailyReport }) => {
+                const { ok, message, path } = exportDailyReport;
+                if (ok) {
+                    setFileLoading(true);
+                    exportFile(path, message, toast).then(() => {
+                        setFileLoading(false);
+                    });
+                }
+            },
+            onError: (err) => {
+                console.log(err);
+                defaultErrorToast(toast);
+            },
+            fetchPolicy: 'network-only',
+        }
+    );
 
     return (
         <Flex {...tableViewContainerStyle} gap={'10px'}>
@@ -46,19 +96,50 @@ export default function Report(props: { siteId: string; siteName: string }) {
                                 leftIcon={<LaunchIcon />}
                                 variant={'buttonGrayOutline'}
                                 h={'36px'}
+                                onClick={() => {
+                                    const dailyIds = Object.values(
+                                        dailyData
+                                    ).flatMap((value) =>
+                                        value.isChecked ? value.dailyId : []
+                                    );
+                                    dailyIds.length > 0 &&
+                                        exportDailyReport({
+                                            variables: {
+                                                dailyIds: dailyIds,
+                                                siteId: siteId,
+                                                username: username,
+                                            },
+                                        });
+                                }}
                             >
                                 輸出
                             </Button>
-                            <DateRangePicker placement="auto" />
+                            <DateRangePicker
+                                value={dateRange}
+                                style={{
+                                    border: '2px solid #919AA9',
+                                    borderRadius: '6px',
+                                }}
+                                onChange={(value) => {
+                                    setDateRange(value);
+                                }}
+                                placement="auto"
+                            />
                         </Flex>
                     )}
                 </Flex>
                 <TabPanels>
                     <TabPanel>
-                        <DailyReport />
+                        <DailyReport
+                            siteId={siteId}
+                            dailyData={dailyData}
+                            setDailyData={setDailyData}
+                            dateRange={dateRange}
+                        />
                     </TabPanel>
                 </TabPanels>
             </Tabs>
+            {(fileLoading || exportDailyLoading) && <PageLoading />}
         </Flex>
     );
 }
