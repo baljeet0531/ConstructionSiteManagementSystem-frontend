678587b93fea10a968b03e022c788cd1e9bb07b8
lhleung <grandq33769@gmail.com>
2023-05-01 17:01:11 +0800

    feat(faultform): init

--- src/Components/EHSFaultForm/EHSFaultForm.tsx
+++ src/Components/EHSFaultForm/EHSFaultForm.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { IsPermit } from '../../Mockdata/Mockdata';
+import { Button, useDisclosure } from '@chakra-ui/react';
+import FaultFormModal from './Modal';
 
 export default function EHSFaultForm() {
     if (!IsPermit('ehs_fault_form')) return <Navigate to="/" replace={true} />;
+    const { onOpen, onClose, isOpen } = useDisclosure();
 
-    return <p>職安衛管理/工安缺失單</p>;
+    return (
+        <>
+            <Button onClick={onOpen}>Test</Button>
+            <FaultFormModal
+                siteId={'M522C0008'}
+                faultId={1}
+                onClose={onClose}
+                isOpen={isOpen}
+            />
+        </>
+    );
 }
---
+++ src/Components/EHSFaultForm/Styles.tsx
@@ -0,0 +1,45 @@
+export const titleStyle = {
+    display: 'flex',
+    alignItems: 'flex-end',
+};
+
+export const subTitleStyle = {
+    fontSize: '24px',
+    display: 'flex',
+    alignItems: 'center',
+};
+
+export const placeholderStyle = {
+    color: '#667080',
+    opacity: 0.5,
+};
+
+export const disabledStyle = {
+    background: 'rgba(102, 112, 128, 0.1)',
+    color: '#667080',
+    border: '0px',
+};
+
+export const contentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+};
+
+export const tableTitleStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#919AA9',
+    borderTop: '#919AA9 solid 1px',
+    borderLeft: '#919AA9 solid 1px',
+    borderBottom: '#919AA9 solid 1px',
+    color: '#FFFFFF',
+};
+
+export const tableContentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderLeft: '#919AA9 solid 1px',
+    borderBottom: '#919AA9 solid 1px',
+};
