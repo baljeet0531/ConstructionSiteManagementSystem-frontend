a3b97ddf8b3068be6e1042d116c339faa58d4043
lhleung <grandq33769@gmail.com>
2023-06-04 17:29:49 +0800

    feat(EHSForm): add abstract class of handler

---
+++ src/Utils/EHSForm/Handler.tsx
@@ -0,0 +1,80 @@
+import { DocumentNode } from 'graphql';
+import { getSignature } from '../../Interface/Signature';
+import {
+    IEHSForm,
+    IEHSFormFillItem,
+    IEHSSignature,
+    IGQLEHSSignature,
+    signatureColNames,
+    SignatureName,
+    SignaturesStateItem,
+} from '../../Interface/EHSForm/Common';
+import { IEHSFormNormal } from '../../Interface/EHSForm/Normal';
+import { IEHSFormSpecial } from '../../Interface/EHSForm/Special';
+
+export abstract class EHSFormHandler {
+    siteId: string;
+    day: string;
+    signatures: Record<SignatureName, SignaturesStateItem>;
+    abstract queryName: string;
+    abstract query: DocumentNode;
+    abstract mutationName: string;
+    abstract mutation: DocumentNode;
+    // eslint-disable-next-line no-unused-vars
+    abstract items: Record<string, IEHSFormFillItem>;
+
+    constructor(
+        siteId: string,
+        day: string,
+        signatures: Record<SignatureName, SignaturesStateItem>
+    ) {
+        this.siteId = siteId;
+        this.day = day;
+        this.signatures = signatures;
+    }
+
+    getInitialValues(): IEHSForm {
+        return {
+            siteId: this.siteId,
+            day: this.day,
+        };
+    }
+
+    parse(data: IEHSForm[]): IEHSForm | undefined {
+        if (!data[0]) return;
+        const t = { ...data[0] };
+
+        for (let i = 0; i < signatureColNames.length; i++) {
+            this.setSigns(i, t);
+        }
+
+        return t;
+    }
+
+    marshal(submitValues: IEHSFormNormal | IEHSFormSpecial) {
+        console.log(submitValues);
+    }
+
+    setSigns(idx: number, form: IEHSForm) {
+        const key = signatureColNames[idx];
+        const [, setSignature] = this.signatures[key];
+        const GQLsigns = form[key] as IGQLEHSSignature[] | undefined;
+        if (GQLsigns) {
+            const signs: IEHSSignature[] = [];
+            const getSignList = async () => {
+                for (let GQLsign of GQLsigns) {
+                    const sign = await getSignature(GQLsign);
+                    signs.push({
+                        ...sign,
+                        day: GQLsign.day,
+                        corpName: GQLsign.corpName,
+                        signatureType: GQLsign.signatureType,
+                    });
+                }
+            };
+            getSignList().then(() => {
+                setSignature(signs);
+            });
+        }
+    }
+}
