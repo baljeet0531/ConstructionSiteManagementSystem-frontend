65987c079f827f6f4dfe9d09f246dcb94f5655f9
baljeet0531 <barryfang0531@gmail.com>
2023-06-05 16:46:12 +0800

    feat(photo-uploader): use reat-webcam and create photoForm layout

--- src/Components/PhotoUploader/PhotoUploader.tsx
+++ src/Components/PhotoUploader/PhotoUploader.tsx
@@ -4,11 +4,17 @@ import Background from '../../Images/WhiteLoginBackground.svg';
 import { useQuery } from '@apollo/client';
 import { IAccountSite, QUERY_ACCOUNT_SITES } from '../../Layouts/Layout';
 import { Cookies } from 'react-cookie';
+import { Camera } from './Camera';
+import PhotoUploadForm from './PhotoUploadForm';
+
+export const SiteIdContext = React.createContext<string | undefined>(undefined);
 
 export default function PhotoUploader() {
     const username: string = new Cookies().get('username');
 
     const [accountSites, setAccountSites] = React.useState<IAccountSite[]>([]);
+    const [imageList, setImageList] = React.useState<string[]>([]);
+    const [selectedSite, setSelectedSite] = React.useState<string>();
 
     const siteOptions = accountSites.map((site, index) => (
         <option key={index} value={site.siteId}>
@@ -34,16 +40,45 @@ export default function PhotoUploader() {
             w={'100vw'}
             h={'100vh'}
             direction={'column'}
-            bgSize={'cover'}
+            align={'center'}
             bgImage={`url(${Background})`}
-            p={'20px 30px'}
+            overflowY={'auto'}
         >
-            <Text w={'400'} fontSize={'1.5rem'}>
-                上傳照片
-            </Text>
-            <Select mt={'10px'} variant={'grayOutline'}>
-                {siteOptions}
-            </Select>
+            <Flex
+                w={'390px'}
+                direction={'column'}
+                align={'center'}
+                p={'30px 20px'}
+                gap={'10px'}
+            >
+                <Text w={'400'} fontSize={'1.5rem'}>
+                    上傳照片
+                </Text>
+                <Select
+                    value={selectedSite}
+                    variant={'grayOutline'}
+                    onChange={(e) => {
+                        setSelectedSite(e.target.value);
+                    }}
+                >
+                    {siteOptions}
+                </Select>
+                <Camera setImageList={setImageList} />
+
+                <SiteIdContext.Provider value={selectedSite}>
+                    <Flex
+                        direction={'column'}
+                        align={'center'}
+                        justify={'center'}
+                        gap={'10px'}
+                        w={'100%'}
+                    >
+                        {imageList.map((src, index) => (
+                            <PhotoUploadForm key={index} imageSrc={src} />
+                        ))}
+                    </Flex>
+                </SiteIdContext.Provider>
+            </Flex>
         </Flex>
     );
 }
