ceed0a04909b2c170cb5a6d22458a70ed9477976
lhleung <grandq33769@gmail.com>
2023-06-10 17:36:21 +0800

    feat(faultForm): complete all function

--- src/Interface/FaultForm.tsx
+++ src/Interface/FaultForm.tsx
@@ -1,9 +1,41 @@
-import { FileType } from "rsuite/esm/Uploader";
+import { FileType } from 'rsuite/esm/Uploader';
 
 export interface IFaultForm {
     siteId: string;
-    faultId: number;
-    checked: boolean;
-    validDate: string;
-    photos: FileType[];
+    day: string;
+    responsibleTarget: string;
+    code: string;
+    staff: string | null;
+    area: string | null;
+    jobNumber: string | null;
+    serialNumber: number | null;
+    department: string | null;
+    issueDay: string | null;
+    responsibleUnitStatus: boolean | null;
+    description: string | null;
+    promptFix: boolean | null;
+    fixDeadline: string | null;
+    fixReport: boolean | null;
+    faultPoint: number | null;
+    reviewDate: string | null;
+    reviewer: string | null;
+    reviewResult: boolean | null;
+    images: { edges: IFaultFormImage[] } | (File | undefined)[];
+    uploaderImages: FileType[];
 }
+
+export interface IFaultFormImage {
+    node: { path: string };
+}
+
+export const stringKeys: (keyof IFaultForm)[] = [
+    'staff',
+    'area',
+    'jobNumber',
+    'department',
+    'issueDay',
+    'description',
+    'fixDeadline',
+    'reviewDate',
+    'reviewer',
+];
