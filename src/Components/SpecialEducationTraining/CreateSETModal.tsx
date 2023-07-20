dfa4b8b332d52161a46180e2fd16705321fb8466
baljeet0531 <barryfang0531@gmail.com>
2023-07-20 16:31:44 +0800

    fix(create-SET-modal): loading humanresource in modal

--- src/Components/SpecialEducationTraining/CreateSETModal.tsx
+++ src/Components/SpecialEducationTraining/CreateSETModal.tsx
@@ -12,7 +12,7 @@ import { defaultErrorToast } from '../../Utils/DefaultToast';
 import dayjs from 'dayjs';
 import { DatePicker, InputPicker } from 'rsuite';
 import { ItemDataType } from 'rsuite/esm/@types/common';
-import { PageLoading } from '../Shared/Loading';
+import { CustomLoading, PageLoading } from '../Shared/Loading';
 import { TEmptyResult } from '../../Hooks/UseGQLOverview';
 import {
     IQuerySpecialEducationalTraining,
@@ -108,95 +108,99 @@ export default function CreateSETModal(props: {
                 onClose={onClose}
                 modalSize={'sm'}
             >
-                <Flex direction={'column'} gap={'20px'}>
-                    <Flex
-                        gap={'12px'}
-                        align={'center'}
-                        justify={'space-between'}
-                    >
-                        <Text
-                            variant={'w400s14'}
-                            style={{ textAlignLast: 'justify' }}
-                            flex={1}
+                {loading ? (
+                    <CustomLoading />
+                ) : (
+                    <Flex direction={'column'} gap={'20px'}>
+                        <Flex
+                            gap={'12px'}
+                            align={'center'}
+                            justify={'space-between'}
                         >
-                            項次
-                        </Text>
-                        <Select
-                            variant={'grayOutline'}
-                            width={'160px'}
-                            fontSize={'14px'}
-                            value={item}
-                            onChange={(e) => {
-                                setItem(e.target.value as TItem);
-                            }}
+                            <Text
+                                variant={'w400s14'}
+                                style={{ textAlignLast: 'justify' }}
+                                flex={1}
+                            >
+                                項次
+                            </Text>
+                            <Select
+                                variant={'grayOutline'}
+                                width={'160px'}
+                                fontSize={'14px'}
+                                value={item}
+                                onChange={(e) => {
+                                    setItem(e.target.value as TItem);
+                                }}
+                            >
+                                {ItemsOptions}
+                            </Select>
+                        </Flex>
+                        <Flex
+                            gap={'12px'}
+                            align={'center'}
+                            justify={'space-between'}
                         >
-                            {ItemsOptions}
-                        </Select>
+                            <Text
+                                variant={'w400s14'}
+                                style={{ textAlignLast: 'justify' }}
+                                flex={1}
+                            >
+                                日期
+                            </Text>
+                            <DatePicker
+                                value={date}
+                                cleanable={false}
+                                oneTap
+                                style={{
+                                    width: '160px',
+                                    border: '2px solid',
+                                    borderColor: '#919AA9',
+                                    borderRadius: '4px',
+                                    background: '#FFFFFF',
+                                    color: '#667080',
+                                }}
+                                onChange={(date) => date && setDate(date)}
+                            />
+                        </Flex>
+                        <Flex
+                            gap={'12px'}
+                            align={'center'}
+                            justify={'space-between'}
+                        >
+                            <Text
+                                variant={'w400s14'}
+                                style={{ textAlignLast: 'justify' }}
+                                flex={1}
+                            >
+                                姓名
+                            </Text>
+                            <InputPicker
+                                style={{
+                                    width: '160px',
+                                    border: '2px solid',
+                                    borderColor: '#919AA9',
+                                    borderRadius: '4px',
+                                    background: '#FFFFFF',
+                                    color: '#667080',
+                                }}
+                                data={data}
+                                value={idno}
+                                onChange={setIdno}
+                                renderMenuItem={(label, item) => {
+                                    return (
+                                        <Flex direction={'column'}>
+                                            <Text>{label}</Text>
+                                            <Text>{item.value}</Text>
+                                        </Flex>
+                                    );
+                                }}
+                            />
+                        </Flex>
                     </Flex>
-                    <Flex
-                        gap={'12px'}
-                        align={'center'}
-                        justify={'space-between'}
-                    >
-                        <Text
-                            variant={'w400s14'}
-                            style={{ textAlignLast: 'justify' }}
-                            flex={1}
-                        >
-                            日期
-                        </Text>
-                        <DatePicker
-                            value={date}
-                            cleanable={false}
-                            oneTap
-                            style={{
-                                width: '160px',
-                                border: '2px solid',
-                                borderColor: '#919AA9',
-                                borderRadius: '4px',
-                                background: '#FFFFFF',
-                                color: '#667080',
-                            }}
-                            onChange={(date) => date && setDate(date)}
-                        />
-                    </Flex>
-                    <Flex
-                        gap={'12px'}
-                        align={'center'}
-                        justify={'space-between'}
-                    >
-                        <Text
-                            variant={'w400s14'}
-                            style={{ textAlignLast: 'justify' }}
-                            flex={1}
-                        >
-                            姓名
-                        </Text>
-                        <InputPicker
-                            style={{
-                                width: '160px',
-                                border: '2px solid',
-                                borderColor: '#919AA9',
-                                borderRadius: '4px',
-                                background: '#FFFFFF',
-                                color: '#667080',
-                            }}
-                            data={data}
-                            value={idno}
-                            onChange={setIdno}
-                            renderMenuItem={(label, item) => {
-                                return (
-                                    <Flex direction={'column'}>
-                                        <Text>{label}</Text>
-                                        <Text>{item.value}</Text>
-                                    </Flex>
-                                );
-                            }}
-                        />
-                    </Flex>
-                </Flex>
+                )}
             </BlueBodyModal>
-            {(loading || updateLoading) && <PageLoading />}
+            {updateLoading && <PageLoading />}
         </>
     );
 }
